48f34027a0fa0afac1ea4ea4349ae6b6867b4c33
Vijayakrishna <vijay@vijay-laptop.(none)>
2011-12-15 20:16:44 -0700

    International

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,10 +11,26 @@ Bodybay::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  # Don't actually send emails
+  config.action_mailer.delivery_method = :test
+  #
+  # Alternate configuration example, using gmail:
+  #   config.action_mailer.delivery_method = :smtp
+  #   config.action_mailer.smtp_settings = {
+  #     address:        "smtp.gmail.com",
+  #     port:           587, 
+  #     domain:         "domain.of.sender.net",
+  #     authentication: "plain",
+  #     user_name:      "dave",
+  #     password:       "secret",
+  #     enable_starttls_auto: true
+  #   } 
+
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
